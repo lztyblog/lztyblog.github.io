4369544893d2372e29bf157f68bf21f7bfc048c3
lztyblog <148581304+lztyblog@users.noreply.github.com>
2025-10-20 15:24:27 +0100

    Add files via upload

---
+++ Array_task.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+int Array(sum) {
+	int64_t array[numbers] = i;
+	int i;
+	
+	for(i=0;i<numbers;i++){
+		sum=sum + i++;
+	}
+	return 0;
+}
+int main(void) {
+	int64_t array[10] = {1,2,3,4,5,6,7,8,9};
+	int totil;
+	totil = Array(sum);
+
+	return 0;
+}
+
+
